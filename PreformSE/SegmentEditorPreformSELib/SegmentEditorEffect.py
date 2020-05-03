474b0bc17d2d17c789136e0aa5d4a505fd6149aa
Jan Witowski <jwitos@gmail.com>
2020-05-03 03:58:58 -0400

    Connect inputs to script Change icon path Get visible segment Choose behavior based on inputs

--- PreformSE/SegmentEditorPreformSELib/SegmentEditorEffect.py
+++ PreformSE/SegmentEditorPreformSELib/SegmentEditorEffect.py
@@ -5,7 +5,8 @@ import subprocess
 from SegmentEditorEffects import *
 
 class SegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
-  """This effect uses Watershed algorithm to partition the input volume"""
+  """This module-effect allows to export the segmentation directly
+  to the PreForm (Formlabs) 3D printing software"""
 
   def __init__(self, scriptedEffect):
     scriptedEffect.name = 'PreformSE'
@@ -22,32 +23,40 @@ class SegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
 
   def icon(self):
     # It should not be necessary to modify this method
-    iconPath = "D:\\Formlabs.jpg"
-    #iconPath = os.path.join(os.path.dirname(__file__), 'SegmentEditorEffect.png')
+    iconPath = os.path.join(os.path.dirname(__file__), 'PreformIcon.jpg')
     if os.path.exists(iconPath):
       return qt.QIcon(iconPath)
     return qt.QIcon()
 
   def helpText(self):
     return """Selected segment is exported as 3D model to PreForm.
-This effect automatically generates a 3D surface model and opens it in Formlabs PreForm, official Formlabs software
-for 3D printing setup and management.
-"""
+
+    This effect automatically generates a 3D surface model and opens it in
+    Formlabs PreForm, official Formlabs software for 3D printing setup and management.
+
+    Only selected (active) segment from the current (active) Segmentation
+    will be exported.
+    """
 
   def setupOptionsFrame(self):
     # PreForm path
     self.preformPath = ctk.ctkPathLineEdit()
     self.scriptedEffect.addLabeledOptionsWidget("Preform Path:", self.preformPath)
 
+    # Select segments
+    self.mergeAllVisibleSegments = qt.QCheckBox()
+    self.mergeAllVisibleSegments.checked = 0
+    self.scriptedEffect.addLabeledOptionsWidget("Merge&export all visible segments:", self.mergeAllVisibleSegments)
+
     # PreForm start flags
     self.enableAutoRepairCheckBox = qt.QCheckBox()
     self.enableAutoRepairCheckBox.checked = 1
     self.scriptedEffect.addLabeledOptionsWidget("Auto Repair:", self.enableAutoRepairCheckBox)
 
-    # Apply button
+    # Export button
     self.applyButton = qt.QPushButton("Apply")
     self.applyButton.objectName = self.__class__.__name__ + 'Apply'
-    self.applyButton.setToolTip("Accept previewed result")
+    self.applyButton.setToolTip("Export segment(s) to PreForm")
     self.scriptedEffect.addOptionsWidget(self.applyButton)
     self.applyButton.connect('clicked()', self.onApply)
 
@@ -55,30 +64,87 @@ for 3D printing setup and management.
     # Turn off effect-specific cursor for this effect
     return slicer.util.mainWindow().cursor
 
+  def getSegmentsToExport(self):
+    # This method returns which segment(s) are going to be exported
+    # If "merge all" checkbox is set, then get all visible segments IDs
+    # If not, get currently selected (active segment)
+
+    if self.mergeAllVisibleSegments.checked:
+      # Get list of visible segment IDs
+      segmentationNode = self.scriptedEffect.parameterSetNode().GetSegmentationNode()
+      visibleSegmentIds = vtk.vtkStringArray()
+      segmentationNode.GetDisplayNode().GetVisibleSegmentIDs(visibleSegmentIds)
+      logging.info("Visible segments:")
+      logging.info(visibleSegmentIds.GetNumberOfValues())
+      if visibleSegmentIds.GetNumberOfValues() == 0:
+        logging.info("No visible segments")
+        return
+      logging.info("First value:")
+      logging.info(visibleSegmentIds.GetValue(0))
+      return visibleSegmentIds
+    else:
+      # Currently selected (active) segment
+      segmentEditorWidget = slicer.modules.segmenteditor.widgetRepresentation().self().editor
+      segmentEditorNode = segmentEditorWidget.mrmlSegmentEditorNode()
+      selectedSegment = segmentEditorNode.GetSelectedSegmentID()
+      return selectedSegment
+
   def onApply(self):
     logging.info('Processing started')
 
-    # Get list of visible segment IDs, as the effect ignores hidden segments.
+    # Get values of checkboxes
+    logging.info(self.mergeAllVisibleSegments.checked)
+    logging.info(self.enableAutoRepairCheckBox.checked)
+
+    # Get path in the input:
+    logging.info(self.preformPath.currentPath)
+
+    # Get segments to export
+    segmentsToExport = self.getSegmentsToExport()
+    logging.info("Segments to export: ")
+    logging.info(segmentsToExport)
+
+    # STL will be exported to a current working directory
+    exportPath = os.getcwd()
+    logging.info("Exporting to")
+    logging.info(exportPath)
+
+    stlFilename = "PreForm_Temporary.stl"
+    stlFilepath = os.path.join(exportPath, stlFilename)
+
     segmentationNode = self.scriptedEffect.parameterSetNode().GetSegmentationNode()
-    visibleSegmentIds = vtk.vtkStringArray()
-    segmentationNode.GetDisplayNode().GetVisibleSegmentIDs(visibleSegmentIds)
-    if visibleSegmentIds.GetNumberOfValues() == 0:
-      logging.info("Smoothing operation skipped: there are no visible segments")
-      return
+    # Export segmentation nodes/segments to STL
+    """slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsClosedSurfaceRepresentationToFiles(
+      destinationFolder=exportPath,
+      segmentationNode=segmentationNode,
+      segmentIds=None,
+      fileFormat="STL",
+      lps=True,
+      sizeScale=1.0,
+      merge=False
+    )"""
 
-    exportPath = "D:\\"
-
-    # Export to STL
-    slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsClosedSurfaceRepresentationToFiles(
-      exportPath,
-      segmentationNode,
-      None
-    )
-
-    # D:\Programs\Formlabs\PreForm\PreForm.exe
-    # Get paths to STL models
-    stlFilename = "Segmentation_Angio_Segment_1.stl"
-    stlFilepath = exportPath + stlFilename
+    if self.mergeAllVisibleSegments.checked:
+      # All segments
+      slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsClosedSurfaceRepresentationToFiles(
+        exportPath,
+        segmentationNode,
+        None,
+        "STL",
+        True,
+        1.0,
+        True
+      )
+    else:
+      slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsClosedSurfaceRepresentationToFiles(
+        exportPath,
+        segmentationNode,
+        segmentsToExport,
+        "STL",
+        True,
+        1.0,
+        False
+      )
 
     # Process params
     params = ""
@@ -88,65 +154,10 @@ for 3D printing setup and management.
     # Open Preform
     logging.info("Opening PreForm")
     #p = subprocess.check_output(preformPath + " " + stlFilepath + params)
-    preformPath = "D:\\Programs\\Formlabs\\PreForm\\PreForm.exe"
-    p = subprocess.Popen(preformPath + " " + stlFilepath + params)
+    # Opening Preform commented out until rest is ready
+    subprocessCommand = f"{self.preformPath.currentPath} '{stlFilepath}' {params}"
+    logging.info(subprocessCommand)
+    p = subprocess.Popen(subprocessCommand)
     logging.info(p)
 
     logging.info('Processing completed')
-
-
-    """# Get list of visible segment IDs, as the effect ignores hidden segments.
-    segmentationNode = self.scriptedEffect.parameterSetNode().GetSegmentationNode()
-    visibleSegmentIds = vtk.vtkStringArray()
-    segmentationNode.GetDisplayNode().GetVisibleSegmentIDs(visibleSegmentIds)
-    if visibleSegmentIds.GetNumberOfValues() == 0:
-      logging.info("Smoothing operation skipped: there are no visible segments")
-      return
-
-    # This can be a long operation - indicate it to the user
-    qt.QApplication.setOverrideCursor(qt.Qt.WaitCursor)
-
-    # Allow users revert to this state by clicking Undo
-    self.scriptedEffect.saveStateForUndo()
-
-    # Export master image data to temporary new volume node.
-    # Note: Although the original master volume node is already in the scene, we do not use it here,
-    # because the master volume may have been resampled to match segmentation geometry.
-    import vtkSegmentationCorePython as vtkSegmentationCore
-    masterVolumeNode = slicer.vtkMRMLScalarVolumeNode()
-    slicer.mrmlScene.AddNode(masterVolumeNode)
-    masterVolumeNode.SetAndObserveTransformNodeID(segmentationNode.GetTransformNodeID())
-    slicer.vtkSlicerSegmentationsModuleLogic.CopyOrientedImageDataToVolumeNode(self.scriptedEffect.masterVolumeImageData(), masterVolumeNode)
-    # Generate merged labelmap of all visible segments, as the filter expects a single labelmap with all the labels.
-    mergedLabelmapNode = slicer.vtkMRMLLabelMapVolumeNode()
-    slicer.mrmlScene.AddNode(mergedLabelmapNode)
-    slicer.vtkSlicerSegmentationsModuleLogic.ExportSegmentsToLabelmapNode(segmentationNode, visibleSegmentIds, mergedLabelmapNode, masterVolumeNode)
-
-    # Run segmentation algorithm
-    import SimpleITK as sitk
-    import sitkUtils
-    # Read input data from Slicer into SimpleITK
-    labelImage = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(mergedLabelmapNode.GetName()))
-    backgroundImage = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(masterVolumeNode.GetName()))
-    # Run watershed filter
-    featureImage = sitk.GradientMagnitudeRecursiveGaussian(backgroundImage, float(self.scriptedEffect.doubleParameter("ObjectScaleMm")))
-    del backgroundImage
-    f = sitk.MorphologicalWatershedFromMarkersImageFilter()
-    f.SetMarkWatershedLine(False)
-    f.SetFullyConnected(False)
-    labelImage = f.Execute(featureImage, labelImage)
-    del featureImage
-    # Pixel type of watershed output is the same as the input. Convert it to int16 now.
-    if labelImage.GetPixelID() != sitk.sitkInt16:
-      labelImage = sitk.Cast(labelImage, sitk.sitkInt16)
-    # Write result from SimpleITK to Slicer. This currently performs a deep copy of the bulk data.
-    sitk.WriteImage(labelImage, sitkUtils.GetSlicerITKReadWriteAddress(mergedLabelmapNode.GetName()))
-    mergedLabelmapNode.GetImageData().Modified()
-    mergedLabelmapNode.Modified()
-
-    # Update segmentation from labelmap node and remove temporary nodes
-    slicer.vtkSlicerSegmentationsModuleLogic.ImportLabelmapToSegmentationNode(mergedLabelmapNode, segmentationNode, visibleSegmentIds)
-    slicer.mrmlScene.RemoveNode(masterVolumeNode)
-    slicer.mrmlScene.RemoveNode(mergedLabelmapNode)
-
-    qt.QApplication.restoreOverrideCursor()"""
